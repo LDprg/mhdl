a03f2a8ffa31ccef44a4253dfccb2bf44cd19246
LDprg <lukas_4dr@gmx.at>
2025-03-27 14:08:53 +0100

    even more code

--- src/lex.zig
+++ src/lex.zig
@@ -5,7 +5,7 @@ pub const Ast = struct {
     scopes: ?[]Scope,
 };
 
-const ScopeTag = enum {
+pub const ScopeTag = enum {
     input,
     output,
     testing,
@@ -14,11 +14,28 @@ const ScopeTag = enum {
 };
 
 pub const Scope = union(ScopeTag) {
-    input: []const u8,
-    output: []const u8,
+    input: ?[]Variable,
+    output: ?[]Variable,
     testing: []const u8,
     logic: []const u8,
     process: struct { []const u8, []const u8 },
+};
+
+pub const Access = enum {
+    global,
+    local,
+};
+
+pub const VariableType = enum { bool };
+pub const VariableDefault = union(VariableType) {
+    bool: bool,
+};
+
+pub const Variable = struct {
+    global: Access,
+    name: []const u8,
+    type: VariableType,
+    default: []const u8,
 };
 
 pub const scopes = scope.many(.{}).map(mecha.toStruct(Ast));
@@ -30,32 +47,44 @@ const scope_input = mecha.combine(.{
     mecha.string("input").discard(),
     ws,
     lcurly,
-    chars,
+    ws,
+    variable.many(.{}),
+    ws,
     rcurly,
+    ws,
 }).map(mecha.unionInit(Scope, ScopeTag.input));
 const scope_ouput = mecha.combine(.{
     ws,
     mecha.string("output").discard(),
     ws,
     lcurly,
-    chars,
+    ws,
+    variable.many(.{}),
+    ws,
     rcurly,
+    ws,
 }).map(mecha.unionInit(Scope, ScopeTag.output));
 const scope_testing = mecha.combine(.{
     ws,
     mecha.string("testing").discard(),
     ws,
     lcurly,
+    ws,
     chars,
+    ws,
     rcurly,
+    ws,
 }).map(mecha.unionInit(Scope, ScopeTag.testing));
 const scope_logic = mecha.combine(.{
     ws,
     mecha.string("logic").discard(),
     ws,
     lcurly,
+    ws,
     chars,
+    ws,
     rcurly,
+    ws,
 }).map(mecha.unionInit(Scope, ScopeTag.logic));
 const scope_process = mecha.combine(.{
     ws,
@@ -64,19 +93,65 @@ const scope_process = mecha.combine(.{
     chars,
     ws,
     lcurly,
+    ws,
     chars,
+    ws,
     rcurly,
+    ws,
 }).map(mecha.unionInit(Scope, ScopeTag.process));
+
+pub const variable = mecha.combine(.{
+    ws,
+    name.convert(mecha.toEnum(Access)),
+    ws,
+    name,
+    ws,
+    colon,
+    ws,
+    name.convert(mecha.toEnum(VariableType)),
+    ws,
+    mecha.opt(mecha.combine(.{
+        equal,
+        ws,
+        number,
+    })),
+    ws,
+    comma,
+    ws,
+}).map(mecha.toStruct(Variable));
+
+const name = mecha.combine(.{
+    nameChars,
+    nameStartChar,
+});
+const nameChars = mecha.oneOf(.{
+    nameStartChar,
+    digit,
+}).many(.{});
+const nameStartChar = mecha.oneOf(.{
+    mecha.utf8.range('a', 'z'),
+    mecha.utf8.range('A', 'Z'),
+    mecha.utf8.char('_'),
+});
 
 const chars = char.many(mecha.ManyOptions{ .collect = false });
 const char = mecha.utf8.range(0x0, 0x7a);
 
-const lcurly = token(mecha.utf8.char('{'));
-const rcurly = token(mecha.utf8.char('}'));
+const number = mecha.combine(.{
+    mecha.opt(sign),
+    digit,
+});
+const sign =
+    mecha.oneOf(.{ plus, minus });
+const digit = mecha.utf8.range('0', '9');
 
-fn token(comptime parser: anytype) mecha.Parser(void) {
-    return mecha.combine(.{ parser.discard(), ws });
-}
+const lcurly = mecha.utf8.char('{').discard();
+const rcurly = mecha.utf8.char('}').discard();
+const colon = mecha.utf8.char(':').discard();
+const comma = mecha.utf8.char(',').discard();
+const equal = mecha.utf8.char('=').discard();
+const minus = mecha.utf8.char('-').discard();
+const plus = mecha.utf8.char('+').discard();
 
 const ws = mecha.oneOf(.{
     mecha.utf8.char(0x0020), // Space
