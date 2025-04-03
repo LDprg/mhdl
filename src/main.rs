aa3bac8af87b68dd95bcc2b049dd4d6779499665
LDprg <lukas_4dr@gmx.at>
2025-04-03 15:09:08 +0200

    move to a token based system
    The transplier shoudl work with following pipeline:
    lexer -> parser -> code generator
    
    lexer: parses code into basic token repesentation (text to structure)
    parser:  creates a ast from tokens (structure to logic repr.)
    code generator: produces verilog code from ast (logic repr. to verilog)
    
    There might be additional steps in the future

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,77 @@
-use std::{env, fs};
+use std::{env, fmt, fs};
 
 use chumsky::prelude::*;
 
+pub type Span = SimpleSpan;
+pub type Spanned<T> = (T, Span);
+
 #[derive(Clone, Debug)]
-#[allow(dead_code)]
-enum Scope {
-    Input(String),
-    Output(String),
-    Test(String),
-    Logic(String),
-    Process(String),
+enum Token<'a> {
+    // Values
+    Bool(bool),
+    Num(i64),
+
+    // Tokens
+    Ident(&'a str),
+    Op(&'a str),
+    Ctrl(char),
+
+    // Types
+    BoolType,
 }
 
-fn parser<'a>() -> impl Parser<'a, &'a str, Vec<Scope>, extra::Err<Simple<'a, char>>> {
-    recursive(|_| {
-        let scope = none_of("{}")
-            .repeated()
-            .to_slice()
-            .map(ToString::to_string)
-            .delimited_by(just('{').padded().ignored(), just('}').padded().ignored());
-        choice((
-            just("input")
-                .ignored()
-                .then(scope)
-                .map(|(_, data)| Scope::Input(data)),
-            just("output")
-                .ignored()
-                .then(scope)
-                .map(|(_, data)| Scope::Output(data)),
-            just("test")
-                .ignored()
-                .then(scope)
-                .map(|(_, data)| Scope::Test(data)),
-            just("logic")
-                .ignored()
-                .then(scope)
-                .map(|(_, data)| Scope::Logic(data)),
-            just("process")
-                .ignored()
-                .then(scope)
-                .map(|(_, data)| Scope::Process(data)),
-        ))
+impl fmt::Display for Token<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            // Values
+            Token::Bool(x) => write!(f, "{}", x),
+            Token::Num(n) => write!(f, "{}", n),
+            // Tokens
+            Token::Ident(s) => write!(f, "{}", s),
+            Token::Op(s) => write!(f, "{}", s),
+            Token::Ctrl(c) => write!(f, "{}", c),
+            // Types
+            Token::BoolType => write!(f, "bool"),
+        }
+    }
+}
+
+fn lexer<'a>() -> impl Parser<'a, &'a str, Vec<Spanned<Token<'a>>>, extra::Err<Rich<'a, char, Span>>>
+{
+    let num = text::int(10)
+        .to_slice()
+        .from_str()
+        .unwrapped()
+        .map(Token::Num);
+
+    let op = one_of("&|!=")
+        .repeated()
+        .at_least(1)
+        .to_slice()
+        .map(Token::Op);
+    let ctrl = one_of("(){}[];,:").map(Token::Ctrl);
+
+    let ident = text::ascii::ident().map(|ident: &str| match ident {
+        "bool" => Token::BoolType,
+        "true" => Token::Bool(true),
+        "false" => Token::Bool(false),
+        _ => Token::Ident(ident),
+    });
+
+    let token = num.or(op).or(ctrl).or(ident);
+
+    let comment = just("//")
+        .then(any().and_is(just('\n').not()).repeated())
+        .padded();
+
+    token
+        .map_with(|tok, e| (tok, e.span()))
+        .padded_by(comment.repeated())
+        .padded()
+        // If we encounter an error, skip and attempt to lex the next character as a token instead
+        .recover_with(skip_then_retry_until(any().ignored(), end()))
         .repeated()
         .collect()
-    })
 }
 
 fn main() {
@@ -53,7 +81,7 @@ fn main() {
         .expect("No file path");
     let src = fs::read_to_string(path).expect("Failed to read file");
 
-    match parser().parse(src.trim()).into_result() {
+    match lexer().parse(src.trim()).into_result() {
         Ok(ast) => ast.into_iter().for_each(|data| println!("{:?}", data)),
         Err(errs) => errs.into_iter().for_each(|e| println!("{:?}", e)),
     };
